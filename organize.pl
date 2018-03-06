7436e0d0001d4045b7f9e4c42ca623881a779be1
Nathan Campos <eeepc904@gmail.com>
2018-03-06 09:22:48 -0300

    Learning to ignore

--- organize.pl
+++ organize.pl
@@ -3,17 +3,44 @@ use strict;
 use warnings;
 use IO::Handle;
 use Term::ANSIColor;
-use FindBin;
-use lib $FindBin::Bin;
+
+# Checks if a model is valid or is just a glorified comment or a reference to another model.
+sub is_valid {
+	my ($line) = @_;
+
+	if ($line =~ m/ako:/) {
+		# Looks like we have a reference model. Lazy bastards...
+		return 0;
+	} elsif (length($line) <= 65) {
+		# This model is less than 65 characters, there is no way this is real.
+		return 0;
+	}
+	
+	return 1;
+}
+
+# Ignores the model and does everything that is needed to dispose of it.
+sub ignore_model {
+	my ($file, $line) = @_;
+
+	print "[" . colored("IGNORE", "red") . "] Line " . $file->input_line_number() . ": $line\n";
+}
 
 my $filename = "models/testing/bjt.txt";
 open(my $fh, "<:encoding(UTF-8)", $filename)
 	or die "Could not open file '$filename': $!";
 
 while (my $line = <$fh>) {
-	if ($line =~ /^\.model/i) {
-		print "[" . colored("ADD", "green") . "] $line";
+	chomp $line;
+
+	if ($line =~ m/^\.model/i) {
+		if (is_valid $line) {
+			print "[" . colored("ADD", "green") . "] $line\n";
+		} else {
+			ignore_model($fh, $line);
+		}
 	} else {
-		print "[" . colored("IGNORE", "red") . "] Line " . $fh->input_line_number() . ": " . $line;
+		ignore_model($fh, $line);
 	}
 }
+
