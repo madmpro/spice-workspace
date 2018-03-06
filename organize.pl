3a3fa28cd4d74184d0d246127e4f71ccb3b0a32e
Nathan Campos <eeepc904@gmail.com>
2018-03-06 23:12:58 +0000

    Checking if the model directory exists

--- organize.pl
+++ organize.pl
@@ -4,6 +4,9 @@ use warnings;
 use IO::Handle;
 use Term::ANSIColor;
 
+my $ignore_log = 1;  # Enable logging ignored lines.
+my $models_dir = "models";  # Location of the model library.
+
 # Checks if a model is valid or is just a glorified comment or a reference to another model.
 sub is_valid {
 	my ($line) = @_;
@@ -32,6 +35,8 @@ my $filename = "models/testing/bjt.txt";
 open(my $fh, "<:encoding(UTF-8)", $filename)
 	or die "Could not open file '$filename': $!";
 
+my $family_dir = "bjt";
+
 while (my $line = <$fh>) {
 	chomp $line;
 
@@ -43,8 +48,15 @@ while (my $line = <$fh>) {
 			$type =~ s/\(.*//;
 			$type = lc($type);
 			$mpn = uc($mpn);
+			my $loc = "$models_dir/$family_dir/$type";
+			my $model_file = lc("$mpn.mod");
 
-			print "[" . colored("ADD", "green") . "] $type - $mpn\n";
+			if (!-d $loc) {
+				print "[" . colored("MKDIR", "bright_yellow") . "] New model family " . uc($type) . ": $loc created.\n";
+				# TODO: Create the actual directory.
+			}
+
+			print "[" . colored("ADD", "green") . "] $type - $mpn @ $loc/$model_file\n";
 		} else {
 			ignore_model($fh, $line);
 		}
